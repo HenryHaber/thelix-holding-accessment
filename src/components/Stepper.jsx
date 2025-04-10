38af06f9e72e3acc8014ab2699f743a3d06cb327
Oramabo Emmanuel <oramabo.emmanuel@gmail.com>
2025-04-10 18:08:59 +0100

    updated app

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -10,29 +10,16 @@ const steps = [
 
 const Stepper = ({ activeStep }) => {
   return (
-      <Flex justify="space-between" align="center" my={6} position="relative">
+      <div className={'flex  gap-4 overflow-auto'}>
         {steps.map((step, i) => (
-            <Flex direction="column" align="center" key={i}>
-              {/*<Flex*/}
-              {/*    justify="center"*/}
-              {/*    align="center"*/}
-              {/*    bg={i === activeStep ? "red.500" : "gray.200"}*/}
-              {/*    color={i === activeStep ? "white" : "black"}*/}
-              {/*    w={8}*/}
-              {/*    h={8}*/}
-              {/*    rounded="full"*/}
-              {/*    fontWeight="bold"*/}
-              {/*    mb={1}*/}
-              {/*>*/}
-              {/*  {i + 1}*/}
-              {/*</Flex>*/}
-              {/*<h4 className={'py-3 px-3 border-solid border-2 border-gray-100 rounded-full'}>*/}
-              {/*  {i + 1}*/}
-              {/*</h4>*/}
-              <Text fontSize="xs" color="gray.500">{step.label}</Text>
-            </Flex>
+            <div className={'flex flex-col items-center '} key={i}>
+              <h4 className={'p-5  w-20  h-20 items-center flex t border-solid border-2 border-gray-100 rounded-full justify-center'}>
+                {i + 1}
+              </h4>
+              <p className={'text-nowrap'}>{step.label}</p>
+            </div>
         ))}
-      </Flex>
+      </div>
   );
 };
 
