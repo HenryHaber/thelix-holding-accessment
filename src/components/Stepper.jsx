a70d2bc06dd790b71fa653fd99e0551eb09e38ad
Oramabo Emmanuel <oramabo.emmanuel@gmail.com>
2025-04-10 18:22:55 +0100

    updated app

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -10,13 +10,18 @@ const steps = [
 
 const Stepper = ({ activeStep }) => {
   return (
-      <div className={'flex  gap-4 overflow-auto'}>
+      <div className={'flex gap-4 overflow-auto'}>
         {steps.map((step, i) => (
-            <div className={'flex flex-col items-center '} key={i}>
-              <h4 className={'p-5  w-20  h-20 items-center flex t border-solid border-2 border-gray-100 rounded-full justify-center'}>
+            <div
+                key={i}
+            >
+              <h4
+                  className={`flex text-black flex-col items-center p-2 w-20 h-20  border-solid  hover:text-white hover:bg-[#A93636] border-2 border-gray-100 
+                  rounded-full justify-center ${activeStep === i ? 'bg-[#A93636] text-white' : ''}`}
+              >
                 {i + 1}
               </h4>
-              <p className={'text-nowrap'}>{step.label}</p>
+              <p className={`text-nowrap  ${activeStep === i ? 'text-[#A93636] ' : ''} } `}>{step.label}</p>
             </div>
         ))}
       </div>
